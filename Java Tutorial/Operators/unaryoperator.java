1c3d2cd949147ad406a269db36e80ef008912a4a
Arun-Vats <asvats374@gmail.com>
2023-02-22 22:42:20 +0530

    unary operator example

---
+++ Java Tutorial/Operators/unaryoperator.java
@@ -0,0 +1,21 @@
+public class unaryoperator{
+    public static void main(String[]args){
+        int a=12,b=12,c=12,d=12,e=12;
+        a++;
+        System.out.println(a);
+        b--;
+        System.out.println(b);
+        ++c;
+        System.out.println(c);
+        --d;
+        System.out.println(d);
+    }
+}
+
+/*In programming languages, a unary operator is an operator that takes only one operand.  
+
+Unary operators can be applied as either prefix operators or postfix operators.
+
+A prefix unary operator is an operator that comes before its operand. 
+and  post is after its operand.
+*/
