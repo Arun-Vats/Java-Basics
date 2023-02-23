a42f03aa1e128fc2632ffbf3bcf5b7e0ec7bae55
Arun-Vats <asvats374@gmail.com>
2023-02-23 12:08:56 +0530

    leftshiftoperator example

---
+++ Java Tutorial/Operators/leftshiftexample.java
@@ -0,0 +1,10 @@
+public class leftshiftexample {
+    public static void main(String[]args){
+        int a=12;
+        System.out.println(a<<2);//12*2^2
+        System.out.println(a<<3);//12*2^3
+    }
+}
+/* In Java, the left shift operator (<<) is a binary operator that shifts the bits of a 
+number to the left by a specified number of positions.
+*/
