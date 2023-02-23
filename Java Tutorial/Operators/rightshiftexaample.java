9501ffb12d12fc47469363b5a3d93b599df00b7c
Arun-Vats <asvats374@gmail.com>
2023-02-23 12:08:28 +0530

    code written for shift operators.

---
+++ Java Tutorial/Operators/rightshiftexaample.java
@@ -0,0 +1,11 @@
+public class rightshiftexaample {
+    public static void main(String[]args){
+        int a=20;
+        System.out.println(a>>>2);    //For positive number, >> and >>> works same  
+        System.out.println(a>>6);
+    }
+}
+/*The Java right shift operator >> is used to move the value of the left operand 
+to right by the number of bits specified by the right operand.
+
+ */
