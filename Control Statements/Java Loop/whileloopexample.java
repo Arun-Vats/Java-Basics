333a72cd95c1caa46b64651d66919e580e9ca5ec
Arun-Vats <asvats374@gmail.com>
2023-02-27 15:02:10 +0530

    for loop and while loop examples

---
+++ Control Statements/Java Loop/whileloopexample.java
@@ -0,0 +1,12 @@
+public class whileloopexample {
+    public static void main(String[]args){
+        int a=12;
+        while(a<=19){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
+/*The while loop is also used to iterate over the number of 
+statements multiple times. However, if we don't know the number 
+of iterations in advance, it is recommended to use a while loop. */
