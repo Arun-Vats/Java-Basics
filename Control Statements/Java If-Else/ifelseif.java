74ba69c515e0729a99796bfbd4e2cee97f50fbdf
Arun-Vats <asvats374@gmail.com>
2023-02-27 09:16:06 +0530

    ifelseif statement example

---
+++ Control Statements/Java If-Else/ifelseif.java
@@ -0,0 +1,20 @@
+public class ifelseif {
+    public static void main(String[]args){
+        String country="India";
+        if(country=="Australia"){
+            System.out.println("Country is Australia.");
+        }
+        else if(country=="Canada"){
+        System.out.println("Country is Canada.");
+        }
+        else if(country=="Germany"){
+        System.out.println("Country is Germany.");
+                    }
+        else if(country=="Canada"){
+        System.out.println("Country is Canada.");
+                    }
+                    else{
+                        System.out.println("Country is India.");
+                    }
+    }
+}
