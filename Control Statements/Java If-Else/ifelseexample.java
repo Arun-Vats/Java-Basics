032af985ea95c5d4fb73c8b1b4e86b0a651949b7
Arun-Vats <asvats374@gmail.com>
2023-02-27 09:08:24 +0530

    ifelse statement examples

---
+++ Control Statements/Java If-Else/ifelseexample.java
@@ -0,0 +1,16 @@
+public class ifelseexample {
+    public static void main(String[]args){
+        int a=12,b=133;
+        if(a>b){
+            System.out.println("a is greater than b.");
+        }
+else {
+    System.out.println("b is greater than a.");
+}
+    }
+}
+/*The if-else statement is an extension to the if-statement, which uses another block of code, 
+i.e., else block. The else block is executed if the condition of the if-block is evaluated as 
+false.
+
+ */
