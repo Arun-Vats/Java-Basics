cb3235b1fcc653c9c6f82c44b31473aeba512767
Arun-Vats <asvats374@gmail.com>
2023-02-27 10:49:28 +0530

    simple switch case example

---
+++ Control Statements/Java Switch/switchexample.java
@@ -0,0 +1,23 @@
+public class switchexample{
+    public static void main(String[]args){
+        int a=12;
+        switch(a){
+            case 1:
+            System.out.println("Value is 1.");
+            break;
+            case 2:
+            System.out.println("Value is 2.");
+            break;
+            case 3:
+            System.out.println("Value is 3.");
+            break;
+            default:
+            System.out.println("Value is 12.");
+        }
+    }
+}
+
+/*In Java, a switch-case statement is a control flow statement 
+that allows a program to evaluate a single variable or 
+expression and then perform different actions based on its value.
+ */
