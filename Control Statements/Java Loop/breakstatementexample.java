bf867411290d3d964101c1e192b002a3f067ddc1
Arun-Vats <asvats374@gmail.com>
2023-02-28 11:26:10 +0530

    break statement example.

---
+++ Control Statements/Java Loop/breakstatementexample.java
@@ -0,0 +1,11 @@
+public class breakstatementexample{
+    public static void main(String[]args){
+        int a=12;
+        for(int i=0;i<=a;i++){
+            System.out.println(i);
+            if(i==10){
+                break;
+            }
+        }
+    }
+}
