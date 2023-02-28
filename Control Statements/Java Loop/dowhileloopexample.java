b9d7fadb698ff8612286a08ca0d7dd2ff045c11e
Arun-Vats <asvats374@gmail.com>
2023-02-28 09:57:45 +0530

    do while loop example to print first 10 even no.

---
+++ Control Statements/Java Loop/dowhileloopexample.java
@@ -0,0 +1,10 @@
+public class dowhileloopexample{
+    public static void main(String[]args){
+        int a=0;
+        do{
+            System.out.println(a);
+            a=a+2;
+        }
+        while(a<=10);
+    }
+}
