7a9134481f4f7e4021ce8dd900d5c7e7260b8a60
Arun-Vats <asvats374@gmail.com>
2023-02-28 22:28:14 +0530

    nested if statement healthy example.

---
+++ Control Statements/Java If-Else/nestedifexample2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class nestedifexample2 {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter Your Age:- ");
+        a=s.nextInt();
+        System.out.println("Enter Your Weight:- ");
+        b=s.nextInt();
+        if(a>18&&a<=45){
+            if(b>48&&b<=70){
+                System.out.println("Congratulations! You Are Healthy.");
+                
+            }
+        }
+        else{
+            System.out.println("Sorry, You Are Not Healthy.");
+        }
+    }
+}
