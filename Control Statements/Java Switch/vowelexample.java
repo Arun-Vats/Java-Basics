290b00470138e3f4b83a6f78983a8d5743b5016b
Arun-Vats <asvats374@gmail.com>
2023-03-01 10:32:11 +0530

    Program to check Vowel or Consonant using switch

---
+++ Control Statements/Java Switch/vowelexample.java
@@ -0,0 +1,24 @@
+public class vowelexample {
+    public static void main(String[]args){
+        char vowel='p';
+        switch(vowel){
+            case 'a':
+            System.out.println("Inserted Character is Vowel.");
+            break;
+            case 'e':
+            System.out.println("Inserted Character is Vowel.");
+            break;
+            case 'i':
+            System.out.println("Inserted Character is Vowel.");
+            break;
+            case 'o':
+            System.out.println("Inserted Character is Vowel.");
+            break;
+            case 'u':
+            System.out.println("Inserted Character is Vowel.");
+            break;
+            default:
+            System.out.println("Inserted Character is consonant.");
+        }
+    }
+}
