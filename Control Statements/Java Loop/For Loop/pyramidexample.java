97d2bd54fb718f9a0243705600f8e822075f4b16
Arun-Vats <asvats374@gmail.com>
2023-03-04 11:49:42 +0530

    pyramid example using nested for loop

--- Control Statements/Java Loop/For Loop/pyramidexample.java
+++ Control Statements/Java Loop/For Loop/pyramidexample.java
@@ -1,10 +1,9 @@
-public class pyramidexample {
-    public static void main(String[]args){
-        for(int i=0;i<=5;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+public class pyramidexample{
+public static void main(String[]args){
+for(int i=0;i<=5;i++){
+    for(int j=0;j<i;j++){
+        System.out.print("*");
     }
+    System.out.println();
 }
+}}
